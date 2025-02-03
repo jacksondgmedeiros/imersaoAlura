d7f389fcc6f8211f79f09d791d6c3ccbad171986
Jackson Medeiros <jackson.dgmedeiros@gmail.com>
2025-02-02 21:29:48 -0300

    finalização do projeto da imersão com o React, falta criar o resto do proejto em react, eles criaram apenas o header e ficou como desafio, o restante, além disso, o projeto deles estava quebrado no css

---
+++ spotify-react/src/Header/Header.js
@@ -0,0 +1,34 @@
+// importa o react para usar toda a estrutura do framework
+import React from "react";
+import './Header.css';
+// para importar as imagens
+import smallLeft from '../assets/icons/small-left.png'
+import smallRight from '../assets/icons/small-right.png'
+import search from "../assets/icons/search.png"
+
+const Header = () => {
+    return (
+        <nav className="header__navigation">
+            <div className="navigation">
+                <button className="arrow_left">
+                    <img src={smallLeft} alt="seta esquerda"/>
+                </button>
+                <button className="arrow_right">
+                    <img src={smallRight} alt="seta direita"/>
+                </button>
+            </div>
+                <div className="header__search">
+                    <img src={search} alt=""/>
+                    <input id="search-input" type="text" maxlength="800" placeholder="O que você quer ouvir?"/>
+                </div>
+
+                <div className="header__login">
+                    <button className="subscribe">Inscreva-se</button>
+                    <button className="login">Entrar</button>
+
+                </div>
+        </nav>
+    )
+};
+
+export default Header;
